826502be2d7705b400a92cec11b7e7d3f8858b83
Jasper Jiang <jasper.jiang@pwc.com>
2018-01-08 17:48:59 +0800

    update

--- my_spider/pipelines.py
+++ my_spider/pipelines.py
@@ -5,10 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import os
 
 
 class MySpiderPipeline(object):
+
     def __init__(self):
+        OUT_DIR = 'out'
+        if not os.path.exists(OUT_DIR):
+            print('Create out folder')
+            os.makedirs(OUT_DIR)
+
         self.fs = open('out/items.txt', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
